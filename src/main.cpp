331a4eafe7b9f0400cec03af4e39903f6df1cecf
Joshua Goldberg <joshuabg@terpmail.umd.edu>
2018-08-19 10:26:35 -0400

    Display temperature on LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 // Data wire is plugged into port 2 on the Arduino
@@ -12,14 +15,24 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
 void setup()
 {
-    // start serial port
     Serial.begin(9600);
-    Serial.println("Dallas Temperature IC Control Library Demo");
 
-    // Start up the library
+    // Init temp sensor library
     sensors.begin();
+
+    // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C (for the 128x64)
+    // init done
 }
 
 void loop()
@@ -32,8 +45,22 @@ void loop()
     // After we got the temperatures, we can print them here.
     // We use the function ByIndex, and as an example get the temperature from the first sensor only.
     Serial.print("Temperature for the device 1 (index 0) is: ");
-    Serial.println(sensors.getTempFByIndex(0));
+    double tempF = sensors.getTempFByIndex(0);
+    Serial.println(tempF);
 
+    display.clearDisplay();
+
+    // text display tests
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("Temperature:");
+    display.println();
+    display.setTextSize(3);
+    display.print(tempF);
+    display.print(" F");
+    display.display();
+
+    // Update every second
     delay(1000);
-
 }
